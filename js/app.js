712a172a721ffb3183edafe000b3d554a5c4b001
Rita O'Rourke <rustiri@yahoo.com>
2020-11-22 22:56:45 -0800

    adding css and images.

--- js/app.js
+++ js/app.js
@@ -2,28 +2,29 @@
 //create const variable: don't want the variable to be reassigned
 const tableData = data;
 
+//alert (tableData);
+
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
 
 function buildTable(data) {
-    //clear any existing data by setting tbody to empty
+    // First, clear out any existing data by setting tbody to empty
     tbody.html("");
-
-    //loop through each object in the data using foreach
-    //It tells JavaScript to find the <tbody> tag within the HTML and add a table row ("tr").
+  
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
     data.forEach((dataRow) => {
-        //create a variable that will append a row to the table body
+        // Append a row to the table body
         let row = tbody.append("tr");
-    });
 
-    //loop through each field in the dataRow argument.
-    //These fields will become table data and 
-    //will be wrapped in <td> tags when they're appended to the HTML table.
-    Object.values(dataRow).forEach((val) => {
-        //create a variable to append data to a table
-        let cell = row.append("td");
-        //add the values
-        cell.text(val);
+        // Loop through each field in the dataRow and add
+        // each value as a table cell (td)
+        Object.values(dataRow).forEach((val) => {
+            //create a variable to append data to a table
+            let cell = row.append("td");
+            //add the values
+            cell.text(val);
+        });
     });
 }
 
